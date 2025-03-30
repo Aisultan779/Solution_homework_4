13f4920c26b072e6c11ab52e3d904e1e4e753408
Aisultan779 <aisultanaidarkul0102@gmail.com>
2025-03-30 20:32:22 +0500

    update

---
+++ src2/ChatServiceAdapter/ChatServiceAdapter.java
@@ -0,0 +1,14 @@
+
+public class ChatServiceAdapter implements ChatService {
+
+    private LegacyChatService legacyChatService;
+
+    public ChatServiceAdapter(LegacyChatService legacyChatService) {
+        this.legacyChatService = legacyChatService;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        legacyChatService.sendLegacyMessage(message);
+    }
+}
